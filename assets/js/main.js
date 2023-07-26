dcf2dabca5579f336ada1b63c3c34b1cd218496b
felipecaos1 <76451966+felipecaos1@users.noreply.github.com>
2023-07-25 21:46:07 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,81 +1,81 @@
-$( document ).ready(function() {
+jQuery( document ).ready(function() {
     
     //efecto hover sobre cada porcio de la esfera
-    $( ".porcion1" ).hover(
+    jQuery( ".porcion1" ).hover(
         function() {
-        $( ".porcion1" ).css("fill",'#072f35');
-        $( ".p1" ).css("fill",'#5d777b');
-        $( ".t1" ).addClass("show");
+        jQuery( ".porcion1" ).css("fill",'#072f35');
+        jQuery( ".p1" ).css("fill",'#5d777b');
+        jQuery( ".t1" ).addClass("show");
     }, function() {
-        $( this ).css("fill",'#3a79f7');
-        $( ".p1" ).css("fill",'#e0e0e0');
-        $( ".t1" ).removeClass("show");
+        jQuery( this ).css("fill",'#3a79f7');
+        jQuery( ".p1" ).css("fill",'#e0e0e0');
+        jQuery( ".t1" ).removeClass("show");
         }
       );
-      $( ".porcion2" ).hover(
+      jQuery( ".porcion2" ).hover(
         function() {
-        $( ".porcion2" ).css("fill",'#072f35');
-        $( ".p2" ).css("fill",'#5d777b');
-        $( ".t2" ).addClass("show");
+        jQuery( ".porcion2" ).css("fill",'#072f35');
+        jQuery( ".p2" ).css("fill",'#5d777b');
+        jQuery( ".t2" ).addClass("show");
         }, function() {
-        $( this ).css("fill",'#3a79f7');
-        $( ".p2" ).css("fill",'#e0e0e0');
-        $( ".t2" ).removeClass("show");
+        jQuery( this ).css("fill",'#3a79f7');
+        jQuery( ".p2" ).css("fill",'#e0e0e0');
+        jQuery( ".t2" ).removeClass("show");
         }
       );
-    $( ".porcion3" ).hover(
+    jQuery( ".porcion3" ).hover(
         function() {
-        $( ".porcion3" ).css("fill",'#072f35');
-        $( ".p3" ).css("fill",'#5d777b');
-        $( ".t3" ).addClass("show");
+        jQuery( ".porcion3" ).css("fill",'#072f35');
+        jQuery( ".p3" ).css("fill",'#5d777b');
+        jQuery( ".t3" ).addClass("show");
         }, function() {
-        $( this ).css("fill",'#3a79f7');
-        $( ".p3" ).css("fill",'#e0e0e0');
-        $( ".t3" ).removeClass("show");
+        jQuery( this ).css("fill",'#3a79f7');
+        jQuery( ".p3" ).css("fill",'#e0e0e0');
+        jQuery( ".t3" ).removeClass("show");
         }
       );
-      $( ".porcion4" ).hover(
+      jQuery( ".porcion4" ).hover(
         function() {
-        $( ".porcion4" ).css("fill",'#072f35');
-        $( ".p4" ).css("fill",'#5d777b');
-        $( ".t4" ).addClass("show");
+        jQuery( ".porcion4" ).css("fill",'#072f35');
+        jQuery( ".p4" ).css("fill",'#5d777b');
+        jQuery( ".t4" ).addClass("show");
         }, function() {
-        $( this ).css("fill",'#3a79f7');
-        $( ".p4" ).css("fill",'#e0e0e0');
-        $( ".t4" ).removeClass("show");
+        jQuery( this ).css("fill",'#3a79f7');
+        jQuery( ".p4" ).css("fill",'#e0e0e0');
+        jQuery( ".t4" ).removeClass("show");
         }
       );
-      $( ".porcion5" ).hover(
+      jQuery( ".porcion5" ).hover(
         function() {
-        $( ".porcion5" ).css("fill",'#072f35');
-        $( ".p5" ).css("fill",'#5d777b');
-        $( ".t5" ).addClass("show");
+        jQuery( ".porcion5" ).css("fill",'#072f35');
+        jQuery( ".p5" ).css("fill",'#5d777b');
+        jQuery( ".t5" ).addClass("show");
         }, function() {
-        $( this ).css("fill",'#3a79f7');
-        $( ".p5" ).css("fill",'#e0e0e0');
-        $( ".t5" ).removeClass("show");
+        jQuery( this ).css("fill",'#3a79f7');
+        jQuery( ".p5" ).css("fill",'#e0e0e0');
+        jQuery( ".t5" ).removeClass("show");
         }
       );
-      $( ".porcion6" ).hover(
+      jQuery( ".porcion6" ).hover(
         function() {
-        $(  ".porcion6" ).css("fill",'#072f35');
-        $( ".p6" ).css("fill",'#5d777b');
-        $( ".t6" ).addClass("show");
+        jQuery(  ".porcion6" ).css("fill",'#072f35');
+        jQuery( ".p6" ).css("fill",'#5d777b');
+        jQuery( ".t6" ).addClass("show");
         }, function() {
-        $( this ).css("fill",'#3a79f7');
-        $( ".p6" ).css("fill",'#e0e0e0');
-        $( ".t6" ).removeClass("show");
+        jQuery( this ).css("fill",'#3a79f7');
+        jQuery( ".p6" ).css("fill",'#e0e0e0');
+        jQuery( ".t6" ).removeClass("show");
         }
       );
-      $( ".porcion7" ).hover(
+      jQuery( ".porcion7" ).hover(
         function() {
-        $( ".porcion7" ).css("fill",'#072f35');
-        $( ".p7" ).css("fill",'#5d777b');
-        $( ".t7" ).addClass("show");
+        jQuery( ".porcion7" ).css("fill",'#072f35');
+        jQuery( ".p7" ).css("fill",'#5d777b');
+        jQuery( ".t7" ).addClass("show");
         }, function() {
-        $( this ).css("fill",'#3a79f7');
-        $( ".p7" ).css("fill",'#e0e0e0');
-        $( ".t7" ).removeClass("show");
+        jQuery( this ).css("fill",'#3a79f7');
+        jQuery( ".p7" ).css("fill",'#e0e0e0');
+        jQuery( ".t7" ).removeClass("show");
         }
       );
 });
